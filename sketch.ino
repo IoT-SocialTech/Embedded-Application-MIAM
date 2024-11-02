ae86e2c52e27c4604b893e87bc053331283a3151
NotLT42 <u20211c667@upc.edu.pe>
2024-11-02 00:21:06 -0500

    Feature: Function Display
    Calling functions to show correct operation in SP32
    
    Co-Authored-By: Rodrigo Raymundo  <130820365+RodrigoRG24@users.noreply.github.com>
    Co-Authored-By: Jean Carlos Achamizo <traxefeder@gmail.com>

--- sketch.ino
+++ sketch.ino
@@ -7,5 +7,15 @@ void setup() {
     device.setup();
 }
 void loop() {
+    device.updateReadings();
 
+    float temperature = device.getTemperature();
+
+    device.lcdClear();
+
+    device.lcdSetCursor(0, 0);
+    device.lcdPrint("Distancia:");
+    device.lcdSetCursor(0, 1);
+    device.lcdPrint(String(distance) + " cm");
+    delay(333);
 }
